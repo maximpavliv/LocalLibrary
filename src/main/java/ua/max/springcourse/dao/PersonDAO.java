53e6267343b0977a7387be21f740491dbac2dff3
maximpavliv <maxim.pavliv@gmail.com>
2024-02-11 17:45:42 +0100

    People CRUD with list as DB

---
+++ src/main/java/ua/max/springcourse/dao/PersonDAO.java
@@ -0,0 +1,43 @@
+package ua.max.springcourse.dao;
+
+import org.springframework.stereotype.Component;
+import ua.max.springcourse.models.Person;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class PersonDAO {
+    private static int PEOPLE_COUNT;
+    private List<Person> people;
+
+    {
+        people = new ArrayList<>();
+        people.add(new Person(++PEOPLE_COUNT, "Esteban"));
+        people.add(new Person(++PEOPLE_COUNT, "Shantam"));
+        people.add(new Person(++PEOPLE_COUNT, "Isma"));
+        people.add(new Person(++PEOPLE_COUNT, "Ester"));
+    }
+
+    public List<Person> index() {
+        return people;
+    }
+
+    public Person show(int id) {
+        return people.stream().filter(person -> person.getId() == id).findAny().orElse(null);
+    }
+
+    public void save(Person person) {
+        person.setId(++PEOPLE_COUNT);
+        people.add(person);
+    }
+
+    public void update(int id, Person updatedPerson) {
+        Person personToUpdate = show(id);
+        personToUpdate.setName(updatedPerson.getName());
+    }
+
+    public void delete(int id) {
+        people.removeIf(person -> person.getId() == id);
+    }
+}
